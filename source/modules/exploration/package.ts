7f0f94cda7244490139cd4a3b0458fa6a41587df
Hursh Patel <hpatel@theaiinstitute.com>
2026-09-17 16:22:43 -0400

    Update package.ts

--- source/modules/exploration/package.ts
+++ source/modules/exploration/package.ts
@@ -2,5 +2,5 @@ export interface Package {
     systemDependencyNames?: string[];
     systemName?: string;
     setupSystemSources?: () => void;
-    postSystemInstall?: () => void;
+    postSystemInstall?: () => Promise<void>;
 }
