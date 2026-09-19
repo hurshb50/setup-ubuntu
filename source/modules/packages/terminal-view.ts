7ccf86cc205a1eaf0df95f3c4d2eae4dad005a29
Hursh Patel <hurshb50@gmail.com>
2026-09-19 01:05:00 -0400

    Add oh-my-posh config asset
    Installer copies assets/oh-my-posh.toml to ~/.config/oh-my-posh/oh-my-posh.toml.

--- source/modules/packages/terminal-view.ts
+++ source/modules/packages/terminal-view.ts
@@ -1,3 +1,5 @@
+import fs from "fs/promises";
+import path from "path";
 import { execa } from "execa";
 import type { InstallContext, Package } from "../package";
 import { Task } from "../task";
@@ -17,6 +19,16 @@ export class TerminalView implements Package {
 
         task.continue("Install oh-my-posh");
         await execa("curl -s https://ohmyposh.dev/install.sh | bash -s", { shell: true });
+
+        const sourceFilePath = path.join(context.directories.assets, "oh-my-posh.toml");
+        const destinationDirectoryPath = path.join(context.directories.home, ".config", "oh-my-posh");
+        const destinationFilePath = path.join(destinationDirectoryPath, "oh-my-posh.toml");
+
+        task.continue("Create configuration directory");
+        await fs.mkdir(destinationDirectoryPath, { recursive: true });
+
+        task.continue("Copy configuration");
+        await fs.copyFile(sourceFilePath, destinationFilePath);
 
         task.finish();
     }
